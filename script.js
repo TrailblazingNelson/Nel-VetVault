5cc4568d938f9dddad10402bb0c7db17e054e3fa
TrailblazingNelson <med.nelsondavid@gmail.com>
2025-06-29 21:09:59 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function openLink(url) {
     window.open(url, '_blank');
 }
+
+function filterSubjects() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const cards = document.getElementsByClassName("subject-card");
+
+    for (let i = 0; i < cards.length; i++) {
+        const title = cards[i].getElementsByTagName("h3")[0].textContent.toLowerCase();
+        
+        if (title.includes(input)) {
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
